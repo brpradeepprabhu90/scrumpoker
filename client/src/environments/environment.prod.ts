15ba14caa41c299fffdc4a7fdfacbbc82ba43923
brpradeepprabhu90 <brpradeepprabhu90@gmail.com>
2022-12-27 11:19:10 -0500

    socket path to https

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: "https://scrumpoker-production.up.railway.app/api/",
-  socketPath: "ws://scrumpoker-production.up.railway.app/ws/pradeep_111_11"
+  socketPath: "wss://scrumpoker-production.up.railway.app/ws/pradeep_111_11"
 };
